a47836a21288add91e8b02eed8c63a294418f425
ag-vectivus <a.s.galecki@gmail.com>
2021-04-30 21:55:27 +0200

    Add localStorage

--- src/contexts/ToDoContext.js
+++ src/contexts/ToDoContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { ToDoReducer } from "../reducers/ToDoReducer";
 
 export const ToDoContext = createContext();
 
 const ToDoContextProvider = (props) => {
-	const [todos, dispatch] = useReducer(ToDoReducer, []);
+	const [todos, dispatch] = useReducer(ToDoReducer, [], () => {
+		const localData = localStorage.getItem("todos");
+		return localData ? JSON.parse(localData) : [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
 
 	return (
 		<ToDoContext.Provider value={{ todos, dispatch }}>
